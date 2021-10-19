a3983fe2843c7cd5aec19c073db327f81e9770ec
Alejandra P. Hernandez <79233777+alepehernandez@users.noreply.github.com>
2021-10-18 23:16:47 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+
+Name: John Smith Date: Jan 1st, 1900 - This is called top level comments
+
+Include top level comments in every assignment
+
+final grade is the average of a midterm and a final exam
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    double base = 10.5;
+    double height = 4.8;
+    double area = base * height/2.0;
+    printf( "This is the base of my triangle = %f\n", base );
+    printf( "This is the height of my triangle = %f\n", height );
+    printf( "The triangle area is = %f\n", area );
+
+return( 0 );
+
+}
