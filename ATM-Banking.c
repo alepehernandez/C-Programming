17108d445424119095fa728a8c178c5d7c1938e3
Alejandra Perez Hernandez <alepehernandez@gmail.com>
2021-11-24 22:13:33 -0800

    work in progress

---
+++ ATM-Banking.c
@@ -0,0 +1,26 @@
+/* Write a C program of a banking system | Work in Progress
+*/
+
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+system ("color 3F");
+
+int pin=1234, option, enteredPin, count=0, amount=1;
+
+float balance =5000;
+int continueTransacion =1;
+
+
+time_t now;
+time (&now);
+printf("\n");
+printf("%s",ctime(&now));
+
+
+
+
+
+    return 0;
+}
