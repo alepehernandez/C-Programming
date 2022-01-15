5e2f82a08b1407023cdae0bd84a447f19e8ff524
Alejandra P. Hernandez <79233777+alepehernandez@users.noreply.github.com>
2022-01-14 21:43:48 -0800

    Update main.c

--- main.c
+++ main.c
@@ -8,16 +8,16 @@ Users input the base and height, and the calculation of the area is given to the
 
 #include <stdio.h>
 int main() {
-    // insert code here...
+    // declare 
     double base=0;
     double height = 0;
-  // Ask user for the base
+  // Ask user to input
     printf("Enter the base of the triangle:\n");
     scanf("%lf",&base);
   // Ask user for the heigh
     printf("Enter the height of the triangle:\n");
     scanf("%lf",&height);
-    // Finding the area
+    // calculation to find the area
     double area = base * height/2.0;
     printf("The triangle area is = %f\n", area);
     return 0;
